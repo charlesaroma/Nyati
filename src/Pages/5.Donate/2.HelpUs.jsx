1e1d5c3cbdf1e570c63da5420e5ad30c8ee0afd2
Charles Aroma <charlesaroma9@gmail.com>
2024-07-06 19:52:41 +0300

    working on donate responsiveness

--- src/Pages/5.Donate/2.HelpUs.jsx
+++ src/Pages/5.Donate/2.HelpUs.jsx
@@ -8,12 +8,12 @@ const HelpUs = () => {
       {/* container */}
       <div className="w-full h-full flex flex-col lg:flex-row justify-between items-center px-[5%] py-16">
         {/* icon, text & button */}
-        <div className="lg:max-w-[41%] flex flex-col justify-center gap-5 lg:pl-[5%]">
+        <div className="w-full lg:max-w-[41%] flex flex-col justify-center gap-5 lg:pl-[5%] lg:text-left text-center">
           {/* icon */}
-          <img src={Icon} alt="icon" className="w-10 h-10" />
+          <img src={Icon} alt="icon" className="w-10 h-10 mx-auto lg:mx-0" />
 
           {/* heading */}
-          <h1 className="text-5xl  text-[#FFFFFE] font-bold">
+          <h1 className="text-3xl lg:text-5xl text-[#FFFFFE] font-bold">
             <span>Help us</span>
             <span className="block">share our stories</span>
           </h1>
@@ -26,18 +26,20 @@ const HelpUs = () => {
           </p>
 
           {/* button */}
-          <button
-            className="bg-[#EE5170] w-[190.34px] h-[41.38px] rounded-[49.66px]
-          font-inter font-semibold text-16 leading-19.36 text-[#FFFAF6] cursor-pointer"
-            type="button"
-          >
-            Send Us A Donation
-          </button>
+          <div className="flex justify-center lg:justify-start">
+            <button
+              className="bg-[#EE5170] w-[190.34px] h-[41.38px] rounded-[49.66px]
+              font-inter font-semibold text-16 leading-19.36 text-[#FFFAF6] cursor-pointer"
+              type="button"
+            >
+              Send Us A Donation
+            </button>
+          </div>
         </div>
 
         {/* image */}
-        <div className="flex items-start">
-          <img src={Image} alt="icon"  />
+        <div className="w-full lg:w-auto flex justify-center mt-8 lg:mt-0">
+          <img src={Image} alt="icon" className="max-w-[80%] lg:max-w-full" />
         </div>
       </div>
     </div>
